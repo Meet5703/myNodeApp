4cc2299da8ef5fa667310db48c54d792e555f155
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2024-01-02 11:12:52 +0530

    3

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ app.use(
 );
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI, {})
   .then(() => {
     console.log("Connected to MongoDB");
   })
