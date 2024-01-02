e967ef44a3604cc3dc0d1c3ddb8484ef3d5f4228
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2024-01-02 20:57:52 +0530

    01

--- models/submission.js
+++ models/submission.js
@@ -6,13 +6,11 @@ const submissionSchema = new mongoose.Schema({
   MotherName: { type: String, required: true },
   Address: { type: String, required: true },
   email: { type: String, required: true },
-  ActingRole: { type: String, required: true }, // Corrected field definition
-  MobileNumber: { type: String, required: true, minlength: 10, maxlength: 10 },
+  ActingRole: { type: String, required: true },
+  MobileNumber: { type: String, required: true },
   WhatsAppNumber: {
     type: String,
     required: true,
-    minlength: 10,
-    maxlength: 10,
   },
   VideoUpload: { type: String },
   createdAt: { type: Date, default: Date.now },
