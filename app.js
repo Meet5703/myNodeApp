b33c5f27643ca4356be27fde0f3c02b711850ab5
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2024-01-02 10:52:15 +0530

    3

--- app.js
+++ app.js
@@ -8,31 +8,29 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3002;
 
-// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/public/uploads",
   express.static(path.join(__dirname, "public/uploads"))
 );
 
-// MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err.message));
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+  });
 
-// Multer storage setup
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./public/uploads");
-  },
+  destination: "./public/uploads",
   filename: (req, file, cb) => {
     cb(
       null,
@@ -42,9 +40,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
-// Routes
-// Route to serve the index.html file
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -64,22 +59,20 @@ app.post("/submitPayment", upload.single("VideoUpload"), async (req, res) => {
       // ... other necessary fields
     } = req.body;
 
-    // Validate if all required fields are present
-    if (
-      !Name ||
-      !FatherName ||
-      !MotherName ||
-      !Address ||
-      !email ||
-      !ActingRole ||
-      !MobileNumber ||
-      !WhatsAppNumber
-      // ... other required fields
-    ) {
+    const requiredFields = [
+      Name,
+      FatherName,
+      MotherName,
+      Address,
+      email,
+      ActingRole,
+      MobileNumber,
+      WhatsAppNumber,
+    ];
+    if (requiredFields.some((field) => !field)) {
       return res.status(400).send("Missing required fields");
     }
 
-    // Create a new submission based on the Submission model/schema
     const newSubmission = new Submission({
       Name,
       FatherName,
@@ -93,7 +86,6 @@ app.post("/submitPayment", upload.single("VideoUpload"), async (req, res) => {
       // ... other necessary fields
     });
 
-    // Save the submission to the database
     const savedSubmission = await newSubmission.save();
 
     console.log("Submission saved:", savedSubmission);
@@ -105,13 +97,13 @@ app.post("/submitPayment", upload.single("VideoUpload"), async (req, res) => {
 });
 
 app.get("/payment", (req, res) => {
-  res.render("payment"); // Assuming you have a payment.ejs file in your views folder
+  res.render("payment");
 });
 
 app.get("/admin", async (req, res) => {
   try {
     const submissions = await Submission.find();
-    res.render("admin", { submissions }); // Pass submissions data to admin.ejs
+    res.render("admin", { submissions });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
@@ -126,7 +118,7 @@ app.get("/admin/download/:id", async (req, res) => {
       return res.status(404).send("Video not found");
     }
 
-    const videoPath = path.join(__dirname, "public", submission.VideoUpload); // Construct the correct file path
+    const videoPath = path.join(__dirname, "public", submission.VideoUpload);
 
     res.set({
       "Content-Type": "video/mp4",
@@ -135,14 +127,13 @@ app.get("/admin/download/:id", async (req, res) => {
       )}"`,
     });
 
-    res.sendFile(videoPath); // Send the file with correct path
+    res.sendFile(videoPath);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
